b0a1faacacdc959d4bec92d3070f126766e5919a
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-01-01 22:01:25 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div>
-      <h2>hello world</h2>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/Projects" element={"Projects"} />
+          <Route path="/contact" element={"Contact"} />
+        </Routes>
+      </Router>
+    </main>
   );
 };
 
