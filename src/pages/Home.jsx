100c81e0f528b3b5f948abcc9c7ce98ab798e173
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-01-04 19:56:15 +0600

    island sence change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Island from "../models/Island";
+import Loader from "../components/Loader";
+
 const Home = () => {
   return (
-    <div>
-      <h1>this is home</h1>
-    </div>
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <mesh />
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island />
+        </Suspense>
+      </Canvas>
+    </section>
   );
 };
 
